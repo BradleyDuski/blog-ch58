7335cefa7eb6b3f7fda920a0a54f205f8bad0187
Bradley Duski <duskibrad@gmail.com>
2025-08-09 14:05:01 -0500

    Blog

---
+++ posts/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "posts/delete.html"
